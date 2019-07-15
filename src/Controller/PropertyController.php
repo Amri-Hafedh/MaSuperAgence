1f0ba28bb1f065f1b960ff87a548de8292a6cc74
Amri <amri_hafedh@yahoo.com>
2019-07-15 22:29:32 +0200

    Property Controller

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class PropertyController{
+
+class PropertyController extends AbstractController
+{
 
 	/**
 	*@Route("Biens", name="property.index")
@@ -12,7 +15,7 @@ class PropertyController{
 	*/
 	public function index():Response{
 
-		return new Response('Les biens');
+		return $this->render('property/index.html.twig', [ 'current_menu'=> 'properties']);
 
 	}
 }
