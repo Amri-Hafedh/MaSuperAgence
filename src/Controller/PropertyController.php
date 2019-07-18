cc09b3ddcc91f4edb3fb8e2df305b907339b2682
Amri <amri_hafedh@yahoo.com>
2019-07-18 21:10:54 +0200

    m

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -1,13 +1,32 @@
 <?php
 
 namespace App\Controller;
+
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Common\EventManager;
+use Doctrine\Common\Persistence\ObjectManager;
 
 
 class PropertyController extends AbstractController
 {
+	/**
+	*@var PropertyRepository
+	*/
+	private $repository;
+	/**
+	*@var ObjectManager
+	*/
+	private $em;
+
+	public function __construct(PropertyRepository $repository, ObjectManager $em){
+
+		$this->repository=$repository; 
+		$this->em = $em;
+	}
 
 	/**
 	*@Route("Biens", name="property.index")
@@ -15,6 +34,7 @@ class PropertyController extends AbstractController
 	*/
 	public function index():Response{
 
+		
 		return $this->render('property/index.html.twig', [ 'current_menu'=> 'properties']);
 
 	}
